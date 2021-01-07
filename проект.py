b16c7c6f50bd4713ce4f8af351fc389edc258b17
MADARA19122006 <nikolayb1206@gmail.com>
2021-01-07 19:48:47 +0300

    1 версия обработки пуль

--- проект.py
+++ проект.py
@@ -3,51 +3,72 @@ import pygame
 
 class Board:
     def __init__(self):
-        self.x = 300
-        self.y = 300
-        self.clock = pygame.time.Clock()
-        self.delay = 0
+        self.m = [[300, 300, 0, 0]]
 
-    def your_tank(self, pos):
+    def draw_tank(self):
         screen = pygame.display.set_mode((1920, 1020))
         screen.fill((0, 150, 0))
-        if pos[0] - self.x == 0:
+        if self.m[0][2] - self.m[0][0] == 0 or self.m[0][3] - self.m[0][1] == 0:
             sin = 0
             cos = 1
         else:
-            tangent = (pos[1] - self.y) / (pos[0] - self.x)
+            tangent = (self.m[0][3] - self.m[0][1]) / (self.m[0][2] - self.m[0][0])
             cos = (1 / (tangent ** 2 + 1)) ** 0.5
             sin = (tangent ** 2 / (tangent ** 2 + 1)) ** 0.5
-        width_gun = 20
-        if self.x > pos[0]:
+        width_gun = 15
+        if self.m[0][0] > self.m[0][2]:
             cos = -cos
-        if self.y > pos[1]:
+        if self.m[0][1] > self.m[0][3]:
             sin = -sin
-        x1 = self.x + cos * 100
-        y1 = self.y + sin * 100
-        pygame.draw.polygon(screen, (0, 0, 0), ((self.x + width_gun * sin, self.y - width_gun * cos),
-                                                (self.x - width_gun * sin, self.y + width_gun * cos),
+        x1 = self.m[0][0] + cos * 80
+        y1 = self.m[0][1] + sin * 80
+        pygame.draw.polygon(screen, (0, 0, 0), ((self.m[0][0] + width_gun * sin, self.m[0][1] - width_gun * cos),
+                                                (self.m[0][0] - width_gun * sin, self.m[0][1] + width_gun * cos),
                                                 (x1 - width_gun * sin, y1 + width_gun * cos),
                                                 (x1 + width_gun * sin, y1 - width_gun * cos)))
-        pygame.draw.circle(screen, (255, 0, 0), (self.x, self.y), 30)
+        pygame.draw.circle(screen, (255, 255, 0), (self.m[0][0], self.m[0][1]), 30)
 
     def move_tank(self):
-        global key, pos
+        global key
         for i in range(len(key)):
-            if key[i] == pygame.K_w:
-                self.y -= 1
-            elif key[i] == pygame.K_s:
-                self.y += 1
-            elif key[i] == pygame.K_a:
-                self.x -= 1
-            elif key[i] == pygame.K_d:
-                self.x += 1
-        self.your_tank(pos)
+            if key[i] == pygame.K_w and self.m[0][1] > 30:
+                self.m[0][1] -= 1
+            elif key[i] == pygame.K_s and self.m[0][1] < 990:
+                self.m[0][1] += 1
+            elif key[i] == pygame.K_a and self.m[0][0] > 30:
+                self.m[0][0] -= 1
+            elif key[i] == pygame.K_d and self.m[0][0] < 1890:
+                self.m[0][0] += 1
+        self.draw_tank()
+
+    def draw_objects(self):
+        k = []
+        for i in range(1, len(self.m)):
+            self.m[i][4] += 1
+            if self.m[i][2] - self.m[i][0] == 0 or self.m[i][3] - self.m[i][1] == 0:
+                sin = 0
+                cos = 1
+            else:
+                tangent = (self.m[i][3] - self.m[i][1]) / (self.m[i][2] - self.m[i][0])
+                cos = (1 / (tangent ** 2 + 1)) ** 0.5
+                sin = (tangent ** 2 / (tangent ** 2 + 1)) ** 0.5
+            if self.m[i][0] > self.m[i][2]:
+                cos = -cos
+            if self.m[i][1] > self.m[i][3]:
+                sin = -sin
+            x1 = self.m[i][0] + cos * self.m[i][4]
+            y1 = self.m[i][1] + sin * self.m[i][4]
+            self.draw_tank()
+            if 5 < x1 < 1915 and 5 < y1 < 1015:
+                pygame.draw.circle(screen, (0, 0, 0), (x1, y1), 10)
+            else:
+                k.append(i)
+            for i in k:
+                del self.m[i]
 
 
 board = Board()
 push = False
-pos = 0
 key = []
 screen = pygame.display.set_mode((1920, 1020))
 screen.fill((0, 150, 0))
@@ -60,7 +81,10 @@ while running:
             running = False
         if event.type == pygame.MOUSEMOTION:
             pos = event.pos
-            board.move_tank()
+            board.m[0] = [board.m[0][0], board.m[0][1], event.pos[0], event.pos[1]]
+            board.draw_tank()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            board.m.append([board.m[0][0], board.m[0][1], event.pos[0], event.pos[1], 0])
         if event.type == pygame.KEYDOWN:
             if event.key in (pygame.K_w, pygame.K_a, pygame.K_s, pygame.K_d):
                 push = True
@@ -73,4 +97,5 @@ while running:
                 del key[key.index(event.key)]
     if push:
         board.move_tank()
+    board.draw_objects()
     pygame.display.flip()
